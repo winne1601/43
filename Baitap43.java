8b6dee8c632c1442e98ad356f219ccdda2b37e7c
winne1601 <97727497+winne1601@users.noreply.github.com>
2022-02-18 16:31:30 +0700

    Add files via upload

---
+++ Baitap43.java
@@ -0,0 +1,14 @@
+	import java.util.Scanner;
+	public class Baitap43 {
+	    public static void main(String[] args){
+	    Scanner sc= new Scanner(System.in);
+	       int a=sc.nextByte();
+	       int b=sc.nextByte();
+	       System.out.println("a+b="+(a+b));
+	       System.out.println("a-b="+(a-b));
+	       System.out.println("a*b="+(a*b));
+	       System.out.println("a/b="+(a/b));
+	       System.out.println("a%b="+(a%b));
+	       System.out.println("a^b="+(a^b));	    
+	    }
+}
